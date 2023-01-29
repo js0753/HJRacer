dbb973f00bb0d30c2dfac49ac55f278d6e12f083
js0753 <jainam728@gmail.com>
2023-01-28 23:37:53 -0500

    Menus

---
+++ Assets/Scripts/ButtonOption.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // UnityEngine.Application.LoadLevel not used anymore
+
+public class ButtonOption : MonoBehaviour {
+
+	public void PlayButton(){
+		SceneManager.LoadScene (2);
+	}
+
+	public void TrackSelect(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void MainMenu(){
+		SceneManager.LoadScene (0);
+	}
+
+	//Below are Track Select Options
+
+	public void Track01(){
+		SceneManager.LoadScene (2);
+	}
+}
