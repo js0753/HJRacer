0a8276b06ebe067fc76b8a9fa5726df768964d3b
js0753 <jainam728@gmail.com>
2023-04-11 04:39:15 -0400

    ScoreMode

---
+++ Assets/Scripts/ModeScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModeScore : MonoBehaviour {
+
+	public int ModeSelection;
+	public GameObject RaceModeUI;
+	public GameObject ScoreModeUI;
+	public GameObject ScoreText;
+	public GameObject AICar;
+	public static int CurrentScore;
+	public int InternalScore;
+	public GameObject ScoreObjects;
+	// Use this for initialization
+	void Start () {
+		ModeSelection = ModeSelect.RaceMode;
+		if (ModeSelection == 1) {
+			RaceModeUI.SetActive (false);
+			ScoreModeUI.SetActive (true);
+			ScoreObjects.SetActive (true);
+			AICar.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InternalScore = CurrentScore;
+		ScoreText.GetComponent<Text> ().text = ""+InternalScore;
+	}
+}
---
+++ Assets/Scripts/ModeSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeSelect : MonoBehaviour {
+
+	public static int RaceMode; //0=Race 1=ScoreMode 2=TimeMode
+
+	public void ScoreMode(){
+		RaceMode = 1;
+	}
+
+	public void TimeMode(){
+		RaceMode = 2;
+	}
+}
