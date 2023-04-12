9eec11b453bb7aa705a520f70b32d7ce1a7a19e1
js0753 <jainam728@gmail.com>
2023-04-12 03:36:02 -0400

    TimeModeUpdatedAttempt

--- Assets/Scripts/ModeTime.cs
+++ Assets/Scripts/ModeTime.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityStandardAssets.Vehicles.Car;
-using System;
 public class ModeTime : MonoBehaviour {
 
 	public int ModeSelection;
@@ -26,6 +25,7 @@ public class ModeTime : MonoBehaviour {
 	public GameObject FinishCam;
 	public GameObject LevelMusic;
 	public GameObject MiniMap;
+	public float countdown;
 	// Use this for initialization
 	void Start () {
 		ModeSelection = ModeSelect.RaceMode;
@@ -43,40 +43,40 @@ public class ModeTime : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		TimeRemaining -= Time.deltaTime * 10;
-		Console.Write (TimeRemaining);
-		if (TimeRemaining > 0) {
-			MilliCount -= Time.deltaTime * 10;
-			if (MilliCount < 0) {
-				MilliCount = 9;
-				SecondCount -= 1;
-			}
-			CountdownMilli.GetComponent<Text> ().text = "" + MilliCount;
-			if (SecondCount <= 9) {
-				CountdownSec.GetComponent<Text> ().text = "0" + SecondCount + ".";
-			} else {
-				CountdownSec.GetComponent<Text> ().text = "" + SecondCount + ".";
-			}
+		countdown += Time.deltaTime;
+		Debug.Log (ModeSelection);
+		if (countdown>=3 && ModeSelection == 2) {
+			TimeRemaining -= Time.deltaTime * 10;
 
-			if (SecondCount < 0) {
-				SecondCount = 59;
-				MinuteCount -= 1;
-			}
-			if (MinuteCount <= 9) {
-				CountdownMin.GetComponent<Text> ().text = "0" + MinuteCount + ":";
-			} else {
-				CountdownMin.GetComponent<Text> ().text = "" + MinuteCount + ":";
-			}
+			if ((int)TimeRemaining >= 0) {
+				MinuteCount = (int)TimeRemaining / (60 * 10);
+				SecondCount = ((int)TimeRemaining - MinuteCount * 600)/10;
+				MilliCount = TimeRemaining - MinuteCount * 600 - SecondCount * 10;
+				CountdownMilli.GetComponent<Text> ().text = "" + MilliCount;
 
-		} else {
-			CarController.m_Topspeed = 0.0f;
-			MyCar.GetComponent<CarController> ().enabled = false;
-			MyCar.GetComponent<CarUserControl> ().enabled = false;
-			MyCar.SetActive (true);
-			FinishCam.SetActive (true);
-			LevelMusic.SetActive (false);
-			CarCam.SetActive (false);
-			MiniMap.SetActive (false);
+				if (SecondCount <= 9) {
+					CountdownSec.GetComponent<Text> ().text = "0" + SecondCount + ".";
+				} else {
+					CountdownSec.GetComponent<Text> ().text = "" + SecondCount + ".";
+				}
+				if (MinuteCount <= 9) {
+					CountdownMin.GetComponent<Text> ().text = "0" + MinuteCount + ":";
+				} else {
+					CountdownMin.GetComponent<Text> ().text = "" + MinuteCount + ":";
+				}
+
+			} else {
+				MyCar.SetActive (false);
+				CarController.m_Topspeed = 0.0f;
+				MyCar.GetComponent<CarController> ().enabled = false;
+				MyCar.GetComponent<CarUserControl> ().enabled = false;
+				MyCar.SetActive (true);
+				FinishCam.SetActive (true);
+				LevelMusic.SetActive (false);
+				CarCam.SetActive (false);
+				MiniMap.SetActive (false);
+				this.enabled = false;
+			}
 		}
 	}
 }
